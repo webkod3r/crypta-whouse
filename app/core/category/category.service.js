3689f7ec84931760795e087e13affaaa02265e4a
Pablo Molina <pablo.molina@zumba.com>
2017-01-20 11:59:03 -0500

    Fixing multiple module definition

--- app/core/category/category.service.js
+++ app/core/category/category.service.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var categoryModule = angular.module('cryptaApp.core.category', []);
+var categoryModule = angular.module('cryptaApp.core.category');
 
 categoryModule.factory('Category', ['$resource', 'appConfig', function ($resource, appConfig) {
   return $resource(appConfig.baseUrl + '/categories/:id', {id: '@_id'}, {
