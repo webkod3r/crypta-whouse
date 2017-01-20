3ab4142894db7d25af8f249c33b9107ba38bd2cd
Pablo Molina <pablo.molina@zumba.com>
2017-01-20 12:50:56 -0500

    Refactoring Secrets List

--- app/secrets/secrets.component.js
+++ app/secrets/secrets.component.js
@@ -3,18 +3,6 @@
 var secretsModule = angular.module('cryptaApp.secrets');
 
 secretsModule.component('secretsList', {
-  templateUrl: 'secrets/secrets.html',
-  controller: ['$scope', 'Secrets', function($scope, Secrets){
-    var self = this;
-    var secrets = Secrets.query();
-    secrets.$promise.then(function (response) {
-      self.secrets = response;
-    });
-    this.order = 'created';
-
-    this.loadSecret = function (secretID) {
-      self.selected = secretID;
-      self.currentSecret = Secrets.get({id: secretID});
-    };
-  }]
+  templateUrl: 'secrets/secrets-list.html',
+  controller: 'SecretsListCtrl'
 });
--- app/secrets/secrets.module.js
+++ app/secrets/secrets.module.js
@@ -4,4 +4,21 @@ var secretsModule= angular.module('cryptaApp.secrets', [
   'ngResource'
 ]);
 
-//secretsModule.controller('SecretsListCtrl', []);
+secretsModule.controller('SecretsListCtrl', ['$scope', 'Secrets', function($scope, Secrets){
+  var self = this;
+  var secrets = Secrets.query();
+  secrets.$promise.then(function (response) {
+    self.secrets = response;
+  });
+  this.order = 'created';
+
+  this.loadSecret = function (secretID) {
+    self.selected = secretID;
+    self.currentSecret = Secrets.get({id: secretID});
+  };
+}]);
+
+// controller to edit the event or add a new one
+secretsModule.controller('SecretFormCtrl', ['$scope', 'Secrets', function ($scope, Secrets) {
+
+}]);
