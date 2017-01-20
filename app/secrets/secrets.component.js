3d4eb547c0c8ffb155bf1e81b763c2d03d3c1d58
Pablo Molina <pablo.molina@zumba.com>
2017-01-20 12:14:46 -0500

    Set active item when is selected

--- app/secrets/secrets.component.js
+++ app/secrets/secrets.component.js
@@ -5,10 +5,16 @@ var secretsModule = angular.module('cryptaApp.secrets');
 secretsModule.component('secretsList', {
   templateUrl: 'secrets/secrets.html',
   controller: ['$scope', 'Secrets', function($scope, Secrets){
-    this.secrets = Secrets.query();
-    this.secrets.$promise.then(function (response) {
-      $scope.secrets = response;
+    var self = this;
+    var secrets = Secrets.query();
+    secrets.$promise.then(function (response) {
+      self.secrets = response;
     });
     this.order = 'created';
+
+    this.loadSecret = function (secretID) {
+      self.selected = secretID;
+      self.currentSecret = Secrets.get({id: secretID});
+    };
   }]
 });
